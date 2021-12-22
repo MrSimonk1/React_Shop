6d6615fcafaeb122007af1669da6bcbcf42434c5
Simonas Krasauskas <simonaskrasauskas@gmail.com>
2021-12-22 09:43:09 +0200

    created toolbar-test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Toolbar from "./components/toolbar";
 
 function App() {
   return (
---
+++ src/components/toolbar.js
@@ -0,0 +1,17 @@
+const Toolbar = () => {
+    return (
+        <div className="toolbar">
+            <div className="homeToolbar">
+
+            </div>
+            <div className="shopToolbar">
+
+            </div>
+            <div className="cartToolbar">
+
+            </div>
+        </div>
+    );
+};
+
+export default Toolbar;
